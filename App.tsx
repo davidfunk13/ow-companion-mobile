cfbc67488e2d6b37814632d5dbbba8611cf9b6b4
davidfunk13 <davefunk135@gmail.com>
2021-08-22 03:38:14 -0600

    goin with UI-kitten and React Navigation. Need to integrate react navigation for drawer

--- App.tsx
+++ App.tsx
@@ -1,81 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
-} from '@react-navigation/drawer';
+import * as eva from "@eva-design/eva";
+import React from "react";
+import { ApplicationProvider, Divider, Drawer, DrawerItem, Icon, IconRegistry, Layout, Text, } from "@ui-kitten/components";
+import { ImageBackground, StyleSheet, } from "react-native";
+import { EvaIconsPack, } from "@ui-kitten/eva-icons";
 
-export default function App() {
-  return (
+const HomeScreen = () => (
+	<Layout style={{
+		flex:           1,
+		justifyContent: "center",
+		alignItems:     "center",
+	}}>
+		<Text category={"h1"}>HOME</Text>
+	</Layout>
+);
 
-    <NavigationContainer>
-      {/* <View style={styles.container}> */}
-        <MyDrawer />
-        {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-        {/* <StatusBar style="auto" /> */}
-      {/* </View> */}
-    </NavigationContainer>
-  );
-}
+const PersonIcon = (props: any) => (
+	<Icon {...props} name={"person-outline"} />
+);
 
-function Feed({ navigation }: any) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed Screen</Text>
-      <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
-      <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
-    </View>
-  );
-}
+const BellIcon = (props: any) => (
+	<Icon {...props} name={"bell-outline"} />
+);
 
-function Notifications() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications Screen</Text>
-    </View>
-  );
-}
+const ForwardIcon = (props: any) => (
+	<Icon {...props} name={"arrow-ios-forward"} />
+);
 
-function CustomDrawerContent(props: any) {
-  return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      <DrawerItem
-        label="Close drawer"
-        onPress={() => props.navigation.closeDrawer()}
-      />
-      <DrawerItem
-        label="Toggle drawer"
-        onPress={() => props.navigation.toggleDrawer()}
-      />
-    </DrawerContentScrollView>
-  );
-}
+const Header = (props: any) => (
+	<React.Fragment>
+		<ImageBackground
+			style={[ props.style, styles.header, ]}
+			source={require("./assets/p.png")}
+		/>
+		<Divider />
+	</React.Fragment>
+);
 
-const Drawer = createDrawerNavigator();
+export const DrawerAccessoriesShowcase = () => {
 
-function MyDrawer() {
-  return (
-    <Drawer.Navigator
-      drawerContent={(props) => <CustomDrawerContent {...props} />}
-    >
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Notifications" component={Notifications} />
-    </Drawer.Navigator>
-  );
-}
+	const [ selectedIndex, setSelectedIndex, ] = React.useState(1);
 
-
+	return (
+		<Drawer
+			header={Header}
+			selectedIndex={selectedIndex as any}
+			onSelect={index => setSelectedIndex(index as any)}>
+			<DrawerItem
+				title={"Users"}
+				accessoryLeft={PersonIcon}
+				accessoryRight={ForwardIcon}
+			/>
+			<DrawerItem
+				title={"Orders"}
+				accessoryLeft={BellIcon}
+				accessoryRight={ForwardIcon}
+			/>
+		</Drawer>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	header: {
+		height:        128,
+		flexDirection: "row",
+		alignItems:    "center",
+	},
 });
+
+const App = () => {
+	return (
+		<>
+			<IconRegistry icons={EvaIconsPack} />
+			< ApplicationProvider {...eva} theme={eva.light} >
+				<DrawerAccessoriesShowcase />
+				<HomeScreen />
+			</ApplicationProvider >
+		</>
+	);
+}
+	;
+
+export default App;
